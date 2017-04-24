d3ce299e65387cb758acb9d88764f06bd342eabd
FSRA <1169659587@qq.com>
2017-04-24 19:36:36 +0800

    Update and rename f.h to insert_sort.h

--- include/f.h
+++
@@ -1,6 +0,0 @@
-#ifndef _FSRA_H
-#define _FSRA_H
-
-int f(int tr,int tc,int dr,int dc,int size);
-
-#endif//_FSRA_H
---
+++ include/insert_sort.h
@@ -0,0 +1,6 @@
+#ifndef _FSRA_H
+#define _FSRA_H
+
+void insert_sort(int a[],int n);
+
+#endif//_FSRA_H
